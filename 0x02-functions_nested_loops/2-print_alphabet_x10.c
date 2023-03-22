95d642d321817a49473e0dd041c18e7af5845a2c
root <danchidera10@gmail.com>
2023-03-22 16:55:47 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * main - Entry point
- * Description:'prints lowercase alphabets 10 times'
- * Return: always 0
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
